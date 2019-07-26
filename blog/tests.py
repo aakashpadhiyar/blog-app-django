3a607c981c976f2d58ea7b13c977ee7ad3990cee
aakash <aakashpadhiyar06@gmail.com>
2019-07-26 18:02:20 +0530

    bugs errors

--- blog/tests.py
+++ blog/tests.py
@@ -21,3 +21,23 @@ class BlogTest(TestCase):
     def test_string_representation(self):
         post = Post(title='A sample title')
         self.assertEqual(str(post), post.title)
+
+
+    def test_post_content(self):
+        self.assertEqual(f'{self.post.title}', 'A goog title')
+        self.assertEqual(f'{self.post.author}', 'testuser')
+        self.assertEqual(f'{self.post.body}', 'Nice body content')
+
+    def test_post_list_view(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response, 'Nice body content')
+        self.assertEqual(response,  'home.html')
+
+    def test_post_detail_view(self):
+        response = self.client.get('/post/1/')
+        no_response = self.client.get('/post/100000')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(no_response.status_code, 404)
+        self.assertEqual(response, 'A good title')
+        self.assertContains(response, 'post_detail.html')
