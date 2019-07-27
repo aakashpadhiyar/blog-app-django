f8d66cb9ab1d97ad63be0ac5c1839d61e322beb2
aakash <aakashpadhiyar06@gmail.com>
2019-07-27 20:38:38 +0530

    Ch5 + Ch6 first commit

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,17 @@
-from django.views  import generic
+from django.views.generic  import ListView, DetailView
+from django.views.generic.edit import CreateView
 from .models import Post
 # Create your views here.
 
-class BlogListView(generic.ListView):
+class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
 
-class BlogDetailView(generic.DetailView):
+class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = '__all__'
