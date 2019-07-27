578912177e4e9ebebec3307ecaaed6b77d5030a5
aakash <aakashpadhiyar06@gmail.com>
2019-07-27 21:08:13 +0530

    Ch5 + Ch6 commit 2

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.views.generic  import ListView, DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from .models import Post
 # Create your views here.
 
@@ -15,3 +15,8 @@ class BlogCreateView(CreateView):
     model = Post
     template_name = 'post_new.html'
     fields = '__all__'
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
